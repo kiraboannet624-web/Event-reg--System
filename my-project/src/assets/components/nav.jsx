f6a26b6838d95adce72c02f90698ac6c6c922734
kiraboannet624 <kiraboannet624@gmail.com>
2026-03-10 22:20:30 +0200

    created form input

---
+++ my-project/src/assets/components/nav.jsx
@@ -0,0 +1,10 @@
+export default function Nav() {
+  return (
+    <nav className="nav">
+      <div className="nav-logo">
+        TECH<span>/</span>CONF<span>_</span>2025
+      </div>
+      <div className="nav-tag">Annual Innovation Summit</div>
+    </nav>
+  );
+}
